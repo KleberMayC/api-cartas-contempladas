602b51de448aba35406a609cfbb0b04ba37b2b12
Kleber Coelho <klebercoelhogt@gmail.com>
2024-03-26 23:02:25 -0300

    #

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cartasRoute = require("./routes/cartasRoute")
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cartasRoute = require("./routes/cartasRoute");
+const cors = require("cors");
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const db = require("../db/models")
+const db = require("../db/models");
 
-app.use('/cartas', cartasRoute)
+app.use("/cartas", cartasRoute);
 
-module.exports = app
+module.exports = app;
